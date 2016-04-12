c916223a7c709e3a77aa556b8cf3f5902d5b906c
Recider <reciderr@gmail.com>
2016-04-12 17:48:12 +0200

    Fixes for .gitignore and manage.py

--- zzn/manage.py
+++ zzn/manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\zzn\\settings.py");
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zzn.settings")
 
     from django.core.management import execute_from_command_line
