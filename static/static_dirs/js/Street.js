20d7135a81b9b087943c54f3ecec3ca561b4086a
Kasper Kalfas <k.kalfas93@gmail.com>
2016-04-13 22:43:15 +0200

    #issue #8 DONE once again, some of the street bug corrected

--- static/static_dirs/js/Street.js
+++ static/static_dirs/js/Street.js
@@ -1,11 +1,11 @@
 var geokoder 	= new google.maps.Geocoder();
-function geocodeLatLng(latlngStr) {
+function geocodeLatLng(latlngStr, place) {
   var latlngStr2 = (String)(latlngStr).split(',',2);
   var latlng = {lat: parseFloat(latlngStr2[0].replace("(", "")), lng: parseFloat(latlngStr2[1].replace(")", ""))};
   geokoder.geocode({'location': latlng}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       if (results[0]) {
-      	alert(results[0].formatted_address);
+        document.getElementById(place).innerHTML = results[0].formatted_address;
         return (results[0].formatted_address);
       } else {
         window.alert('No results found');
